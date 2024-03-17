bbeea4e0949666827ff9b97a194d60171c7e67d4
Ricky Renaldi <rickyrenaldi100@gmail.com>
2024-03-18 04:23:21 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302223051/Program.cs
+++ tpmodul5_1302223051/Program.cs
@@ -6,12 +6,33 @@
         string nama = Console.ReadLine();
 
         HaloGeneric.SapaUser(nama);
+
+        Console.Write("Masukan NIM praktikan : ");
+        string nim = Console.ReadLine();
+
+        DataGeneric<string> dataNIM = new DataGeneric<string>(nim);
+        dataNIM.PrintData();
     }
     public class HaloGeneric()
     {
         public static void SapaUser<T>(T X)
         {
             Console.WriteLine("Halo user " + X);
+        }
+    }
+    public class DataGeneric<T>()
+    {
+        private T data;
+
+        public DataGeneric(T userInput) : this()
+        {
+            data = userInput;
+        }
+
+        public void PrintData()
+        {
+            T Y = data;
+            Console.WriteLine("Data yang tersimpan adalah : " + Y);
         }
     }
 }
